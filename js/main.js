60b1ba08313de150aaceecd6333b85d96be67497
Steven Fitzgerald <eyejabb@gmail.com>
2022-12-30 09:47:11 -0800

    testing initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// function determinePlural(num){
+//     let int = Math.floor(num.length)
+//     for(i = 0; i < int; i++){
+
+    function plural(n) {
+        if (n != 1) {
+          return true;
+        }
+        return false;
+      }
+
+
+//loop through length of the string to see if integer is repeated, aka plural
+//return true if plural
+//return false if not plural
